03ff00e321a3a9938ba0ce7b883f8736f1ef47cc
Mario Auqui <112342340+marioauqui@users.noreply.github.com>
2026-03-13 17:04:44 +0000

    Fri, Mar 13, 2026, 1:04 PM -04:00

---
+++ einstein/einstein.py
@@ -0,0 +1,11 @@
+#ask the user for mass and keep their input
+
+mass = int(input("How much mass?"))
+
+#Then multiply it by 300000000^2
+
+Joules = (pow(300000000,2) * mass)
+
+print("# of joules is" , Joules)
+
+
