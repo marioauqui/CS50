4727f8a0ecaa5ca587a43c375052509e2bf92df9
Mario Auqui <112342340+marioauqui@users.noreply.github.com>
2026-03-15 20:46:16 +0000

    Sun, Mar 15, 2026, 4:46 PM -04:00

--- cat.py
+++ cat.py
@@ -7,7 +7,35 @@
 	#i = i - 1
 
 
-i = 0
-while i < 3:
-	print("Hi!")
-	i += 1
+#i = 0
+#while i < 3:
+#	print("Hi!")
+#	i += 1
+
+
+#while True:
+#	n = int(input("What's n? "))
+#	if n > 0:
+#		break
+
+#for _ in range(n):
+#	print("meow")
+
+def main():
+    number = getnumber()
+    meow(number)
+
+
+def getnumber():
+    while True:
+        n = int(input("Pick a number n (make sure it's positive): " ))
+        if n > 0:
+              return n
+
+def meow(n):
+     for _ in range(n):
+          print("Meow")
+
+main()
+
+
---
+++ hog.py
@@ -0,0 +1,11 @@
+#dictonary/dict
+
+students = {
+	"Herm": "Griff",
+	"Harry": "Griff",
+	"Ron": "Griff",
+	"Draco": "Sly",
+}
+
+for student in students:
+	print(student, students[student], sep = ": ")
