b7f58f9520843d0287834ae881f346a970c9e0e8
Mario Auqui <112342340+marioauqui@users.noreply.github.com>
2026-03-15 18:47:41 +0000

    Sun, Mar 15, 2026, 2:47 PM -04:00

--- cat.py
+++ cat.py
@@ -1,9 +1,13 @@
 
-i = 10
-while i != 0:
+#i = 10
+#while i != 0:
 
-	print("Meow, meow, woof")
+	#print("Meow, meow, woof")
 
-	i = i - 1
+	#i = i - 1
 
 
+i = 0
+while i < 3:
+	print("Hi!")
+	i += 1
